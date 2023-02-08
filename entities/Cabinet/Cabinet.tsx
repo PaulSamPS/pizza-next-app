14155b59f7bd3618dcd7098a59b2ee1229200176
PaulSamPS <psamoylenko2@gmail.com>
2023-02-09 00:38:10 +0300

    cabinet order card

--- entities/Cabinet/Cabinet.tsx
+++ entities/Cabinet/Cabinet.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
-import { Title } from '@components/Typography';
-import { Tab } from '@components/Blocks';
+import { Text, Title } from '@components/Typography';
+import { Card, Divider, Icon, Tab } from '@components/Blocks';
+import { ArrowDownSmallIcon } from '@helpers/icons/16';
 import styles from './Cabinet.module.scss';
 import { CabinetOrderCard } from '../CabinetOrderCard/CabinetOrderCard';
 
 const arr = ['История заказов', 'Настройки'];
+const card = [
+  {
+    id: 0,
+    title: 'Заказ',
+    value: '№130312',
+    date: '22.01.2023',
+  },
+  {
+    id: 1,
+    title: 'Сумма',
+    value: '399 ₽',
+  },
+  {
+    id: 2,
+    title: 'Статус',
+    value: 'Обрабатывается',
+  },
+  {
+    id: 3,
+    title: 'Оплачено',
+    value: 'Картой',
+  },
+];
 
 export const Cabinet = () => {
   const [currentValue, setCurrentValue] = React.useState<string>(arr[0]);
@@ -19,7 +43,29 @@ export const Cabinet = () => {
           setValue={setCurrentValue}
         />
       </div>
-      <CabinetOrderCard />
+      <Card className={styles.card}>
+        <div className={styles.top}>
+          <div className={styles.border} />
+          {card.map((c) => (
+            <CabinetOrderCard
+              key={c.id}
+              title={c.title}
+              value={c.value}
+              date={c.date}
+            />
+          ))}
+          <Icon className={styles.icon}>
+            <ArrowDownSmallIcon />
+          </Icon>
+        </div>
+        <Divider />
+        <div className={styles['order-info']}>
+          <Text level='l2'>
+            ул. Львовская 48/2, офис 301, 2 этаж, домофон 4801#
+          </Text>
+        </div>
+        <Divider />
+      </Card>
     </div>
   );
 };
--- entities/CabinetOrderCard/CabinetOrderCard.tsx
+++ entities/CabinetOrderCard/CabinetOrderCard.tsx
@@ -1,35 +1,31 @@
 import React from 'react';
-import { Card } from '@components/Blocks';
 import { Text } from '@components/Typography';
 import styles from './CabinetOrderCard.module.scss';
 
-export const CabinetOrderCard = () => {
-  return (
-    <div className={styles['order-card']}>
-      <Card>
-        <div className={styles.top}>
-          <div className={styles.border} />
-          <div className={styles.order}>
-            <Text level='l1' className={styles.title}>
-              Заказ
-            </Text>
-            <div className={styles.info}>
-              <Text level='l2' className={styles.number}>
-                №130312
-              </Text>
-              <Text level='l2' className={styles.date}>
-                22.01.23
-              </Text>
-            </div>
-          </div>
-          <div className={styles.sum}>
-            <Text level='l1' className={styles.title}>
-              Сумма
-            </Text>
-            <Text level='l2'>399 ₽</Text>
-          </div>
-        </div>
-      </Card>
+interface CabinetOrderCardProps {
+  title: string;
+  value: string;
+  date?: string;
+}
+
+export const CabinetOrderCard = ({
+  date,
+  value,
+  title,
+}: CabinetOrderCardProps) => (
+  <div className={styles['order-card']}>
+    <Text level='l1' className={styles.title}>
+      {title}
+    </Text>
+    <div className={styles.info}>
+      <Text level='l2' className={styles.number}>
+        {value}
+      </Text>
+      {date && (
+        <Text level='l2' className={styles.date}>
+          22.01.23
+        </Text>
+      )}
     </div>
-  );
-};
+  </div>
+);
