3e258a5cee203f47558bc07fea6179c4f612b23a
PaulSamPS <psamoylenko2@gmail.com>
2023-04-08 00:45:16 +0300

    прижал футер к низу

--- shared/hoc/withLayout/ui/Layout/Layout.tsx
+++ shared/hoc/withLayout/ui/Layout/Layout.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
-import { Header, Nav } from '@entities';
+import { Footer, Header, Nav } from '@entities';
+import styles from './Layout.module.scss';
 
 type LayoutProps = {
   children: React.ReactNode;
 };
 
 export const Layout = ({ children }: LayoutProps) => (
-  <>
-    <Header />
-    <Nav />
-    {children}
-  </>
+  <div className={styles.body}>
+    <div className={styles.content}>
+      <Header />
+      <Nav />
+      {children}
+    </div>
+    <div className={styles.footer}>
+      <Footer />
+    </div>
+  </div>
 );
